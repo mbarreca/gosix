b5c4b340ee1ddb733af03989e9106b43ea9afaa0
Matteo Barreca <matteob11@protonmail.com>
2024-11-20 11:03:06 -0500

    Fixing ConsumerRestriction Model

--- models/plugins_security.go
+++ models/plugins_security.go
@@ -1,12 +1,12 @@
 package models
 
 type ConsumerRestriction struct {
-	Type           string                     `json:"type,omitempty" validate:"omitempty,alphanum,min=1,max=100"`         // Default - consumer_name - Valid Values - consumer_name, consumer_group_id, service_id, route_id - Type of object to base the restriction on.
-	Whitelist      []string                   `json:"whitelist,omitempty" validate:"omitempty,dive"`                      // List of objects to whitelist. Has a higher priority than allowed_by_methods.
-	Blacklist      []string                   `json:"blacklist,omitempty" validate:"omitempty,dive"`                      // List of objects to blacklist. Has a higher priority than whitelist.
-	RejectedCode   int                        `json:"rejected_code,omitempty" validate:"omitempty,number"`                // Default - 403 - Valid Values - HTTP status code returned when the request is rejected.
-	RejectedMsg    string                     `json:"rejected_msg,omitempty" validate:"omitempty,alphanum,min=1,max=250"` // Message returned when the request is rejected.
-	AllowByMethods *RestrictionAllowByMethods `json:"allowed_by_methods,omitempty" validate:"omitempty"`                  // List of allowed configurations for Consumer settings, including a username of the Consumer and a list of allowed HTTP methods.
+	Type           string                       `json:"type,omitempty" validate:"omitempty,alphanum,min=1,max=100"`         // Default - consumer_name - Valid Values - consumer_name, consumer_group_id, service_id, route_id - Type of object to base the restriction on.
+	Whitelist      []string                     `json:"whitelist,omitempty" validate:"omitempty,dive"`                      // List of objects to whitelist. Has a higher priority than allowed_by_methods.
+	Blacklist      []string                     `json:"blacklist,omitempty" validate:"omitempty,dive"`                      // List of objects to blacklist. Has a higher priority than whitelist.
+	RejectedCode   int                          `json:"rejected_code,omitempty" validate:"omitempty,number"`                // Default - 403 - Valid Values - HTTP status code returned when the request is rejected.
+	RejectedMsg    string                       `json:"rejected_msg,omitempty" validate:"omitempty,alphanum,min=1,max=250"` // Message returned when the request is rejected.
+	AllowByMethods []*RestrictionAllowByMethods `json:"allowed_by_methods,omitempty" validate:"omitempty"`                  // List of allowed configurations for Consumer settings, including a username of the Consumer and a list of allowed HTTP methods.
 
 }
 
