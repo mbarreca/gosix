89bbfe21d4269017f52404f53ddd88830e9f6a9e
Matteo Barreca <matteob11@protonmail.com>
2024-12-06 20:30:18 -0500

    Modified Model Restrictions

--- models/plugins_authentication.go
+++ models/plugins_authentication.go
@@ -7,7 +7,7 @@ type BasicAuth struct {
 }
 
 type JwtAuth struct {
-	Key                 string                 `json:"key,omitempty" validate:"required,ascii,min=20,max=200"`         // Unique Key for the Consumer
+	Key                 string                 `json:"key,omitempty" validate:"omitempty,ascii,min=20,max=200"`        // Unique Key for the Consumer
 	Secret              string                 `json:"secret,omitempty" validate:"omitempty,ascii,min=10,max=200"`     // HIGHLY Recommended to use a secret in this instance - The encryption key. If unspecified, auto generated in the background. This field supports saving the value in Secret Manager using the APISIX Secret resource.
 	PublicKey           string                 `json:"public_key,omitempty" validate:"omitempty,ascii,min=10,max=200"` // Required if RS256 or ES256 is set for the algorithm attribute. RSA or ECDSA public key. This field supports saving the value in Secret Manager using the APISIX Secret resource.
 	Algorithm           JwtEncryptionAlgorithm `json:"algorithm,omitempty" validate:"omitempty"`                       // Default is HS256 - ["HS256", "HS512", "RS256", "ES256"] - Encryption algorithm.
@@ -27,7 +27,7 @@ const (
 )
 
 type KeyAuth struct {
-	Key  string `json:"key,omitempty" validate:"required,ascii,min=8,max=200"` // Unique key for a Consumer. This field supports saving the value in Secret Manager using the APISIX Secret resource.
+	Key  string `json:"key,omitempty" validate:"omitempty,ascii,min=8,max=200"` // Unique key for a Consumer. This field supports saving the value in Secret Manager using the APISIX Secret resource.
 	Exp  string `json:"exp,omitempty" validate:"omitempty,ascii,min=15,max=40"`
 	Meta *Meta  `json:"_meta,omitempty" validate:"omitempty"`
 }
